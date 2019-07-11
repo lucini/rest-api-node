672d945c572bbf5f0ffd17c06c2de9fc9f3c428a
rafael.lucini <rafael.lucini@viasoft.com.br>
2019-07-11 11:15:56 -0300

    Json route

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}...`));
 
 app.get('/', (req, res) => {
     res.send('Hello, darkness my old friend.');
+});
+
+app.get('/country', (req, res) => {
+    res.send({id: 1, name: 'Brazil'}).json();
 });
